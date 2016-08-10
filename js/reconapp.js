ede8445479138bae5d7447a06c67682492bcff63
arthurwu1@gmail.com <your_email@example.com>
2016-08-10 11:08:09 +0200

    added nanobar

--- js/reconapp.js
+++ js/reconapp.js
@@ -21,6 +21,23 @@ var result = "";
 var test_item;
 var test_arr = [];
 
+var options = {
+  id: 'nan'
+};
+
+var nanobar = new Nanobar( options );
+
+// move bar
+nanobar.go( 30 ); // size bar 30%
+nanobar.go( 76 ); // size bar 76%
+
+// size bar 100% and and finish
+nanobar.go(100);
+
+
+window.onload = function() {
+    $("#loading-mask").remove();
+};
 
 function makeTableHTML(myArray) {
     var result = "<table border=1>";
